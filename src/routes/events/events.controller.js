eb562d34f1e3eead618c10bfef6f850319b3856b
Kristen Spencer <kristenamspencer@gmail.com>
2020-03-07 11:01:48 -0500

    adds put request and validation

--- src/routes/events/events.controller.js
+++ src/routes/events/events.controller.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const writeFile = promisify(fs.writeFile);
+const { validationResult } = require('express-validator');
 const eventsData = require('../../../db/events.data.json');
 
 const listEvents = (req, res) => {
@@ -9,17 +10,42 @@ const listEvents = (req, res) => {
 };
 
 const postNewEvent = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.empty) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   const id = eventsData.length + 1;
   const newEventsData = [...eventsData, { id, ...req.body }];
   await writeFile('db/events.data.json', JSON.stringify(newEventsData));
   res.status(201);
+
   return res.json({
     id,
     ...req.body
   });
 };
 
+const updateEvent = async (req, res) => {
+  const id = req.params.id;
+  const updatedEventsData = eventsData.map(event => {
+    if (event.id == id) {
+      return {
+        ...event,
+        ...req.body
+      };
+    }
+    return event;
+  });
+
+  await writeFile('db/events.data.json', JSON.stringify(updatedEventsData));
+  res.status(200);
+  return res.json({
+    id
+  });
+};
+
 module.exports = {
   listEvents,
-  postNewEvent
+  postNewEvent,
+  updateEvent
 };
--- src/routes/events/events.router.js
+++ src/routes/events/events.router.js
@@ -1,11 +1,53 @@
 const express = require('express');
+const { check } = require('express-validator');
 
-const { listEvents, postNewEvent } = require('./events.controller');
+const {
+  listEvents,
+  postNewEvent,
+  updateEvent
+} = require('./events.controller');
+const { validateBody } = require('../../middleware/validate-body');
 
 const router = express.Router();
 
 router.get('', listEvents);
-router.post('', postNewEvent);
+
+router.post(
+  '',
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('description')
+      .not()
+      .isEmpty(),
+    check('location')
+      .not()
+      .isEmpty()
+  ],
+  validateBody,
+  postNewEvent
+);
+
+router.put(
+  '/:id',
+  [
+    check('name')
+      .optional()
+      .not()
+      .isEmpty(),
+    check('description')
+      .optional()
+      .not()
+      .isEmpty(),
+    check('location')
+      .optional()
+      .not()
+      .isEmpty()
+  ],
+  validateBody,
+  updateEvent
+);
 
 module.exports = {
   eventsRouter: router
